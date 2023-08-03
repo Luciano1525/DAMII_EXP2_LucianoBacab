453b59f1065a42655ee576f4efdb21d95a05b8ff
Luciano Bacab <lucianobacab427@gmail.com>
2023-08-02 23:38:14 -0600

    Segundo Commit

--- app/src/main/java/com/example/damii_exp2_lucianobacab/Peliculas.java
+++ app/src/main/java/com/example/damii_exp2_lucianobacab/Peliculas.java
@@ -1,14 +1,117 @@
 package com.example.damii_exp2_lucianobacab;
 
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
+import androidx.core.content.ContextCompat;
+import androidx.core.content.FileProvider;
 
+import android.Manifest;
+import android.content.DialogInterface;
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.graphics.Bitmap;
+import android.net.Uri;
 import android.os.Bundle;
+import android.os.Environment;
+import android.provider.MediaStore;
+import android.util.Log;
+import android.view.View;
+import android.widget.ImageView;
+import android.widget.Toast;
+import java.io.File;
+import java.io.IOException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.Locale;
 
 public class Peliculas extends AppCompatActivity {
+    private static final int REQUEST_CAMERA_PERMISSION = 200;
+    private static final int REQUEST_IMAGE_CAPTURE = 1;
+    private ImageView ivPerfil;
+    private Uri photoUri;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_peliculas);
+
+
+        ivPerfil = (ImageView)findViewById(R.id.ivPerfil);
+
+
+        AlertDialog.Builder builder = new AlertDialog.Builder(Peliculas.this);
+        builder.setTitle("Reproduccion");
+        builder.setMessage("Necesita Registrar una Foto para Reproducir la Pelicula \n " +
+                "Desea registrar una Foto?");
+        builder.setCancelable(false);
+
+        builder.setPositiveButton("Registrar Foto", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+                //Registrar Foto
+                if (ContextCompat.checkSelfPermission(Peliculas.this, Manifest.permission.CAMERA)
+                        == PackageManager.PERMISSION_GRANTED) {
+                    // Si el permiso para acceder a la cámara está concedido
+                    openCamera();
+                } else {
+                    // Si no está concedido, solicitamos el permiso
+                    ActivityCompat.requestPermissions(Peliculas.this,
+                            new String[]{Manifest.permission.CAMERA},
+                            REQUEST_CAMERA_PERMISSION);
+                }
+
+                ivPerfil.setVisibility(View.VISIBLE);
+            }
+        });
+
+        builder.setNegativeButton("Cancelar", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+                Log.i("INFO:", "StrimingsApp");
+                Intent intent = new Intent(Peliculas.this, Strimings.class);
+                startActivity(intent);
+
+            }
+        });
+
+        AlertDialog dialog = builder.create();
+        dialog.show();
+
+
     }
+
+    private void openCamera() {
+        Intent takePictureIntent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+        if (takePictureIntent.resolveActivity(getPackageManager()) != null) {
+            startActivityForResult(takePictureIntent, REQUEST_IMAGE_CAPTURE);
+        }
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if (requestCode == REQUEST_IMAGE_CAPTURE && resultCode == RESULT_OK) {
+            if (data != null && data.getExtras() != null) {
+                Bitmap imageBitmap = (Bitmap) data.getExtras().get("data");
+                if (imageBitmap != null) {
+                    ivPerfil.setImageBitmap(imageBitmap);
+                }
+            }
+        }
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        if (requestCode == REQUEST_CAMERA_PERMISSION) {
+            if (grantResults.length > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                openCamera();
+            }
+        }
+    }
+
+
+
+
 }
